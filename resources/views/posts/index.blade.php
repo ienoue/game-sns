2681bbd2a5c23398719bc7f2da302b2b928bbb90
ienoue <gyaagle+github@gmail.com>
2022-03-31 15:40:57 +0900

    bootstrapのカラム幅が狭く、タブが二段になってしまう問題の修正 #56

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-9 col-lg-8">
 
                 {{-- 投稿フォーム --}}
                 @auth
--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-9 col-lg-8">
 
                 {{-- ユーザ情報 --}}
                 @include('inc.userInfo', [
