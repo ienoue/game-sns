e489712308f4c870196315d7e0e06aaf212b1646
ienoue <gyaagle+github@gmail.com>
2022-04-02 21:44:19 +0900

    記事詳細画面用のviewで変数定義が抜けていたのを修正 #74

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,11 @@
         <div class="row justify-content-center">
             <div class="col-md-9 col-lg-8">
                 {{-- 投稿内容 --}}
-                @include('inc.post', ['stretchedLink' => false, 'charLimit' => false])
+                @include('inc.post', [
+                    'stretchedLink' => false,
+                    'charLimit' => false,
+                    'likeBtn' => $post->likeBtnState(),
+                ])
                 {{-- /投稿内容 --}}
             </div>
         </div>
